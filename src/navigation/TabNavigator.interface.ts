05fcd776a7460d120a78b69043e8cb6499f0aa4e
unknown <goodluckxdc@gmail.com>
2023-01-20 21:27:35 -0500

    worth

---
+++ src/navigation/TabNavigator.interface.ts
@@ -0,0 +1,7 @@
+export type TabNavigatorType = {
+    Home: undefined;
+    Profile: undefined;
+    List: undefined;
+    Settings: undefined;
+  };
+  
--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,2 +1,5 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {TabNavigatorType} from './TabNavigator.interface';
+const Tab = createBottomTabNavigator<TabNavigatorType>();
 const Navigator = () => {};
 export default Navigator;
