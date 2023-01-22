f13e4444e3f558e63bf3fc6c1d908cce3e15e7eb
unknown <goodluckxdc@gmail.com>
2023-01-21 21:45:44 -0500

    add

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,4 +1,5 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { FontAwesome5 } from "@expo/vector-icons";
 import {TabNavigatorType} from './TabNavigator.interface';
 const Tab = createBottomTabNavigator<TabNavigatorType>();
 const Navigator = () => {};
