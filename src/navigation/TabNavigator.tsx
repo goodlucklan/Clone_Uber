58f5f79f694f7b077a517dfabd0d9351dc914a67
unknown <goodluckxdc@gmail.com>
2023-01-22 23:02:15 -0500

    any for this time

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -2,16 +2,16 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {FontAwesome5} from '@expo/vector-icons';
 import {TabNavigatorType} from './TabNavigator.interface';
 const Tab = createBottomTabNavigator<TabNavigatorType>();
-const TAB_ICON = {
+const TAB_ICON: any = {
   Home: 'home',
   Profile: 'user-alt',
   List: 'list',
   Settings: 'info',
 };
-const createScreenOptions = ({route}) => {
+const createScreenOptions = ({route}: any) => {
   const iconName = TAB_ICON[route.name];
   return {
-    tabBarIcon: ({size, color}) => (
+    tabBarIcon: ({size, color}: any) => (
       <FontAwesome5 name={iconName} size={size} color={color} />
     ),
   };
