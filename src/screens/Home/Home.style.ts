6bb473e3cfdafcc96851f634ec2203ce32d0b830
unknown <goodluckxdc@gmail.com>
2023-01-23 21:01:48 -0500

    styles home

---
+++ src/screens/Home/Home.style.ts
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 32,
+    justifyContent: 'center',
+  },
+  buttonStyle: {
+    color: '#000000',
+  },
+  textStyle: {
+    fontSize: 24,
+    padding: 8,
+    textAlign: 'center',
+  },
+});
