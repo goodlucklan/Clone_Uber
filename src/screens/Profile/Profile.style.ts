d93347dc0eb0788648d409bf705029bccf712c5b
unknown <goodluckxdc@gmail.com>
2023-01-27 20:24:59 -0500

    profile

---
+++ src/screens/Profile/Profile.style.ts
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 32,
+    justifyContent: 'center',
+  },
+  buttonStyle: {
+    color: '#000000',
+  },
+  textStyle: {
+    fontSize: 24,
+    padding: 8,
+    textAlign: 'center',
+  },
+  colorStyle: {
+    fontSize: 24
+  }
+});
