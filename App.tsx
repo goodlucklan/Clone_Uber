365bb1194f1730741322f9775e9a416546b5e1af
unknown <goodluckxdc@gmail.com>
2023-02-01 21:41:15 -0500

    work calendar

--- App.tsx
+++ App.tsx
@@ -1,18 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {View} from 'react-native';
+import {Agenda, Calendar} from 'react-native-calendars';
+// import {NavigationContainer} from '@react-navigation/native';
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <></>
-    </NavigationContainer>
+    <View
+      style={{
+        flex: 1,
+      }}>
+      <Calendar
+        markingType={'period'}
+        markedDates={{
+          '2023-02-15': {marked: true, dotColor: '#50cebb'},
+          '2023-02-16': {marked: true, dotColor: '#50cebb'},
+          '2023-02-21': {
+            startingDay: true,
+            color: '#50cebb',
+            textColor: 'white',
+          },
+          '2023-02-22': {color: '#70d7c7', textColor: 'white'},
+          '2023-02-23': {
+            color: '#70d7c7',
+            textColor: 'white',
+            marked: true,
+            dotColor: 'white',
+          },
+          '2023-02-24': {color: '#70d7c7', textColor: 'white'},
+          '2023-02-25': {endingDay: true, color: '#50cebb', textColor: 'white'},
+        }}
+      />
+    </View>
   );
 }
 
