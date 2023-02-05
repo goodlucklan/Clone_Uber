78520d2c9707378f0353ade1c3795f7dcf793672
unknown <goodluckxdc@gmail.com>
2023-02-05 17:27:52 -0500

    work

--- App.tsx
+++ App.tsx
@@ -1,37 +1,78 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import {Alert, View, Modal, Text, Pressable, StyleSheet} from 'react-native';
 import {Agenda, Calendar} from 'react-native-calendars';
 // import {NavigationContainer} from '@react-navigation/native';
 
 function App(): JSX.Element {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View
-      style={{
-        flex: 1,
-      }}>
-      <Calendar
-        markingType={'period'}
-        markedDates={{
-          '2023-02-15': {marked: true, dotColor: '#50cebb'},
-          '2023-02-16': {marked: true, dotColor: '#50cebb'},
-          '2023-02-21': {
-            startingDay: true,
-            color: '#50cebb',
-            textColor: 'white',
-          },
-          '2023-02-22': {color: '#70d7c7', textColor: 'white'},
-          '2023-02-23': {
-            color: '#70d7c7',
-            textColor: 'white',
-            marked: true,
-            dotColor: 'white',
-          },
-          '2023-02-24': {color: '#70d7c7', textColor: 'white'},
-          '2023-02-25': {endingDay: true, color: '#50cebb', textColor: 'white'},
-        }}
-      />
+    <View style={{paddingTop: 50, flex: 1}}>
+      <Calendar onDayPress={day => setModalVisible(!modalVisible)} />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
 
 export default App;
